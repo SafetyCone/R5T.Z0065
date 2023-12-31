21b84d11f2fb5352e30f8a2d37770a1934df7dbd
davidcoats <david@getsafetycone.com>
2023-12-31 15:06:33 -0800

    Initial commit.

---
+++ source/R5T.Z0065.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.Z0065.Construction
+Construction console project for the R5T.Z0065 library.
---
+++ source/R5T.Z0065/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0065
+{
+	/// <summary>
+	/// Microsoft namespace name values.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
